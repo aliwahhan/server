8927ee590a82c1e09317a454389434ce14307f9d
wahhan <aliwahha9@gmail.com>
2025-10-05 00:57:36 +0300

    [PM-17562] Refactor event integration methods / declarations in ServiceCollectionExtensions * [PM-17562] Refactor event integration methods / declarations in ServiceCollectionExtensions
    * Refactored ServiceCollectionExtensions to use TryAdd and still launch unique listeneer services
    
    * Updated unit tests to match new generic format for Listeners
    
    * Fix method spacing
    
    * Update README to reflect new integration setup in ServiceCollectionExtensions
    
    * Move interfaces to I prefix; fix typo in subscription
    
    * Fix reference to IIntegrationListenerConfiguration

---
+++ src/Core/AdminConsole/Models/Data/EventIntegrations/RepositoryListenerConfiguration.cs
@@ -0,0 +1,17 @@
+using Bit.Core.Settings;
+
+namespace Bit.Core.AdminConsole.Models.Data.EventIntegrations;
+
+public class RepositoryListenerConfiguration(GlobalSettings globalSettings)
+    : ListenerConfiguration(globalSettings), IEventListenerConfiguration
+{
+    public string EventQueueName
+    {
+        get => _globalSettings.EventLogging.RabbitMq.EventRepositoryQueueName;
+    }
+
+    public string EventSubscriptionName
+    {
+        get => _globalSettings.EventLogging.AzureServiceBus.EventRepositorySubscriptionName;
+    }
+}
