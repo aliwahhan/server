ccd8e12c3424f91287591fa8cbfab00800121f30
wahhan <aliwahha9@gmail.com>
2025-10-05 20:59:25 +0300

    [PM-22108] Add PolicyDetails_ReadByOrganizationId proc (#6019)

---
+++ src/Core/AdminConsole/Models/Data/Organizations/Policies/OrganizationPolicyDetails.cs
@@ -0,0 +1,6 @@
+namespace Bit.Core.AdminConsole.Models.Data.Organizations.Policies;
+
+public class OrganizationPolicyDetails : PolicyDetails
+{
+    public Guid UserId { get; set; }
+}
--- src/Sql/dbo/Tables/OrganizationUser.sql
+++ src/Sql/dbo/Tables/OrganizationUser.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[OrganizationUser] (
+﻿﻿CREATE TABLE [dbo].[OrganizationUser] (
     [Id]                            UNIQUEIDENTIFIER    NOT NULL,
     [OrganizationId]                UNIQUEIDENTIFIER    NOT NULL,
     [UserId]                        UNIQUEIDENTIFIER    NULL,
@@ -17,20 +17,21 @@
     CONSTRAINT [FK_OrganizationUser_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([Id])
 );
 
-
 GO
 CREATE NONCLUSTERED INDEX [IX_OrganizationUser_UserIdOrganizationIdStatusV2]
     ON [dbo].[OrganizationUser]([UserId] ASC, [OrganizationId] ASC, [Status] ASC);
-
-
 GO
+
 CREATE NONCLUSTERED INDEX [IX_OrganizationUser_OrganizationId]
     ON [dbo].[OrganizationUser]([OrganizationId] ASC);
+GO
 
+CREATE NONCLUSTERED INDEX  IX_OrganizationUser_EmailOrganizationIdStatus
+    ON OrganizationUser (Email ASC, OrganizationId ASC, [Status] ASC);
 GO
 
 CREATE NONCLUSTERED INDEX [IX_OrganizationUser_OrganizationId_UserId]
     ON [dbo].[OrganizationUser] ([OrganizationId], [UserId])
-    INCLUDE ([Email], [Status], [Type], [ExternalId], [CreationDate], 
+    INCLUDE ([Email], [Status], [Type], [ExternalId], [CreationDate],
         [RevisionDate], [Permissions], [ResetPasswordKey], [AccessSecretsManager]);
 GO
