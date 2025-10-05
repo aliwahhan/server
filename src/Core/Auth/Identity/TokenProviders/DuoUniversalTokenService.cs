fa7ce414d5f70111db66c1a513866f80d44163e9
wahhan <aliwahha9@gmail.com>
2025-10-05 18:11:38 +0300

    fix(duo): update client header name from Bitwarden to Deepsafer
    - Changed header key from 'Bitwarden-Client-Name' to 'Deepsafer-Client-Name'
    - Ensures Duo auth redirects back to the correct initiating client

--- src/Core/Auth/Identity/TokenProviders/DuoUniversalTokenService.cs
+++ src/Core/Auth/Identity/TokenProviders/DuoUniversalTokenService.cs
@@ -161,9 +161,9 @@ public class DuoUniversalTokenService(
     {
         // Fetch Client name from header value since duo auth can be initiated from multiple clients and we want
         // to redirect back to the initiating client
-        _currentContext.HttpContext.Request.Headers.TryGetValue("Bitwarden-Client-Name", out var bitwardenClientName);
+        _currentContext.HttpContext.Request.Headers.TryGetValue("Deepsafer-Client-Name", out var deepsaferClientName);
         var redirectUri = string.Format("{0}/duo-redirect-connector.html?client={1}",
-            _globalSettings.BaseServiceUri.Vault, bitwardenClientName.FirstOrDefault() ?? "web");
+            _globalSettings.BaseServiceUri.Vault, deepsaferClientName.FirstOrDefault() ?? "web");
 
         var client = new Duo.ClientBuilder(
             (string)provider.MetaData["ClientId"],
