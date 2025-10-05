3ee317f3e9d99513f62d15f5e8351020181e8b57
wahhan <aliwahha9@gmail.com>
2025-10-05 17:12:05 +0300

    Add Windows environment build files with project icons and notification support

---
+++ src/Notifications/build.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = 'Stop'
+$dir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+Write-Host ""
+Write-Host "## Building Notifications"
+
+Write-Host ""
+Write-Host "Building app"
+$dotnetVersion = & dotnet --version
+Write-Host ".NET Core version $dotnetVersion"
+
+$projectPath = Join-Path $dir 'Notifications.csproj'
+$outputDir = Join-Path $dir 'obj/build-output/publish'
+
+Write-Host "Restore"
+& dotnet restore $projectPath
+
+Write-Host "Clean"
+& dotnet clean $projectPath -c "Release" -o $outputDir
+
+Write-Host "Publish"
+& dotnet publish $projectPath -c "Release" -o $outputDir
+
+
