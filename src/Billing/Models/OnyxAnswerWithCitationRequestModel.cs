2cb338fef9674a5492b21b70639ffe8cfb184b7d
wahhan <aliwahha9@gmail.com>
2025-10-06 09:58:02 +0300

    PM-24509 remove limit field (#6169)

--- src/Billing/Models/OnyxAnswerWithCitationRequestModel.cs
+++ src/Billing/Models/OnyxAnswerWithCitationRequestModel.cs
@@ -46,8 +46,6 @@ public class RetrievalOptions
     [JsonPropertyName("real_time")]
     public bool RealTime { get; set; } = true;
 
-    [JsonPropertyName("limit")]
-    public int? Limit { get; set; } = 3;
 }
 
 public class RetrievalOptionsRunSearch
