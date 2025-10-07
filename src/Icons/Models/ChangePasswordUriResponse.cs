ff8d847c90460265f5a2acd4e082488de22e22d7
wahhan <aliwahha9@gmail.com>
2025-10-07 11:50:50 +0300

    PM-21024 ChangePasswordUri controller + service (#5845) * add ChangePasswordUri controller and service to Icons
    * add individual settings for change password uri
    
    * add logging to change password uri controller
    
    * use custom http client that follows redirects
    
    * add ChangePasswordUriService tests
    
    * remove unneeded null check
    
    * fix copy pasta - changePasswordUriSettings
    
    * add HelpUsersUpdatePasswords policy
    
    * Remove policy for change password uri - this was removed from scope
    
    * fix nullable warnings

---
+++ src/Icons/Models/ChangePasswordUriResponse.cs
@@ -0,0 +1,11 @@
+namespace Bit.Icons.Models;
+
+public class ChangePasswordUriResponse
+{
+    public string? uri { get; set; }
+
+    public ChangePasswordUriResponse(string? uri)
+    {
+        this.uri = uri;
+    }
+}
