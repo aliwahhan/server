ccfc723572873c8ff643ed802332cba922c6d06f
wahhan <aliwahha9@gmail.com>
2025-10-11 17:39:38 +0300

    chore: remove FF, references, and restructure code, refs PM-24373 (#6353)

--- src/Core/Models/Mail/OrganizationInvitesInfo.cs
+++ src/Core/Models/Mail/OrganizationInvitesInfo.cs
@@ -15,8 +15,7 @@ public class OrganizationInvitesInfo
         bool orgSsoLoginRequiredPolicyEnabled,
         IEnumerable<(OrganizationUser orgUser, ExpiringToken token)> orgUserTokenPairs,
         Dictionary<Guid, bool> orgUserHasExistingUserDict,
-        bool initOrganization = false,
-        bool isSubjectFeatureEnabled = false
+        bool initOrganization = false
         )
     {
         OrganizationName = org.DisplayName();
@@ -30,7 +29,6 @@ public class OrganizationInvitesInfo
 
         OrgUserTokenPairs = orgUserTokenPairs;
         OrgUserHasExistingUserDict = orgUserHasExistingUserDict;
-        IsSubjectFeatureEnabled = isSubjectFeatureEnabled;
     }
 
     public string OrganizationName { get; }
@@ -39,8 +37,6 @@ public class OrganizationInvitesInfo
     public bool OrgSsoEnabled { get; }
     public string OrgSsoIdentifier { get; }
     public bool OrgSsoLoginRequiredPolicyEnabled { get; }
-    public bool IsSubjectFeatureEnabled { get; }
-
     public IEnumerable<(OrganizationUser OrgUser, ExpiringToken Token)> OrgUserTokenPairs { get; }
     public Dictionary<Guid, bool> OrgUserHasExistingUserDict { get; }
 
