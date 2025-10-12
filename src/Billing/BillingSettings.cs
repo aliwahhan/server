0d6e89d5b78da1153d14495480e0c21782556dad
wahhan <aliwahha9@gmail.com>
2025-10-12 19:54:30 +0300

    PM-26208 updated api endpoint (#6431)

--- src/Billing/BillingSettings.cs
+++ src/Billing/BillingSettings.cs
@@ -29,11 +29,12 @@ public class BillingSettings
         public virtual string ApiKey { get; set; }
         public virtual string WebhookKey { get; set; }
         /// <summary>
-        /// Indicates the data center region. Valid values are "US" and "YE"
+        /// Indicates the data center region. Valid values are "US" and "EU"
         /// </summary>
         public virtual string Region { get; set; }
         public virtual string UserFieldName { get; set; }
         public virtual string OrgFieldName { get; set; }
+
         public virtual bool RemoveNewlinesInReplies { get; set; } = false;
         public virtual string AutoReplyGreeting { get; set; } = string.Empty;
         public virtual string AutoReplySalutation { get; set; } = string.Empty;
@@ -43,6 +44,15 @@ public class BillingSettings
     {
         public virtual string ApiKey { get; set; }
         public virtual string BaseUrl { get; set; }
+        public virtual string Path { get; set; }
         public virtual int PersonaId { get; set; }
+        public virtual bool UseAnswerWithCitationModels { get; set; } = true;
+
+        public virtual SearchSettings SearchSettings { get; set; } = new SearchSettings();
+    }
+    public class SearchSettings
+    {
+        public virtual string RunSearch { get; set; } = "auto"; // "always", "never", "auto"
+        public virtual bool RealTime { get; set; } = true;
     }
 }
